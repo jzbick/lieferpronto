0118a1aa33f6c72de8ea9b93405943bf02062615
THeppner <timo.heppner@itc-studenten.de>
2021-05-30 14:07:33 +0200

    updateOrderStatus added Enums public + save as String in DB

--- src/main/java/com/lieferpronto/lieferpronto/order/components/OrderController.java
+++ src/main/java/com/lieferpronto/lieferpronto/order/components/OrderController.java
@@ -1,7 +1,6 @@
 package com.lieferpronto.lieferpronto.order.components;
 
 import com.lieferpronto.lieferpronto.order.models.Order;
-import com.lieferpronto.lieferpronto.order.models.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
