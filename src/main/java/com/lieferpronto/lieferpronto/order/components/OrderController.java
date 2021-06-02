9519fcf118d2f3b5363ba3d3d9483af8f7fd4ade
THeppner <timo.heppner@itc-studenten.de>
2021-06-02 14:08:47 +0200

    Slug added in Restaurant
    OrderController imports fixed

--- src/main/java/com/lieferpronto/lieferpronto/order/components/OrderController.java
+++ src/main/java/com/lieferpronto/lieferpronto/order/components/OrderController.java
@@ -1,6 +1,7 @@
 package com.lieferpronto.lieferpronto.order.components;
 
 import com.lieferpronto.lieferpronto.order.models.Order;
+import com.lieferpronto.lieferpronto.order.models.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
